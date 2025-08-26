4fa4849d7b63bfa6b787c0074c91a73e0c326469
颜国进 <guojin_yjs@cumt.edu.cn>
2025-08-27 00:03:02 +0800

    Add Yolov8PoseModel

---
+++ src/DeploySharp/Model/Config/Yolo/Yolov8PoseConfig.cs
@@ -0,0 +1,34 @@
+﻿using DeploySharp.Data;
+using DeploySharp.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DeploySharp.Model
+{
+    public class Yolov8PoseConfig : Yolov5DetConfig
+    {
+        public Yolov8PoseConfig() { }
+
+        public Yolov8PoseConfig(string modelPath,
+            InferenceBackend inferenceBackend = InferenceBackend.OpenVINO,
+            DeviceType deviceType = DeviceType.CPU,
+            float confidenceThreshold = 0.5f,
+            float nmsThreshold = 0.5f,
+            int inferBatch = 1,
+            ResizeMode resizeMode = ResizeMode.Pad)
+        {
+            this.ModelPath = modelPath;
+            this.TargetInferenceBackend = inferenceBackend;
+            this.TargetDeviceType = deviceType;
+            this.ConfidenceThreshold = confidenceThreshold;
+            this.NmsThreshold = nmsThreshold;
+            this.InferBatch = inferBatch;
+            this.ImgResizeMode = resizeMode;
+
+        }
+    }
+}
