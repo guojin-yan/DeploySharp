2846b18710d879d3bd863c12e1716c6b99cda3e0
颜国进 <guojin_yjs@cumt.edu.cn>
2025-09-14 13:05:43 +0800

    The same project path.

---
+++ src/DeploySharp.ImageSharp/Model/ModelService/Yolo/Yolov13DetModel.cs
@@ -0,0 +1,43 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeploySharp.Model;
+using DeploySharp.Data;
+using DeploySharp.Log;
+
+
+namespace DeploySharp.Model
+{
+
+    public class Yolov13DetModel : IYolov13DetModel
+    {
+        /// <summary>
+        /// Constructor initializes with model configuration
+        /// </summary>
+        /// <param name="config">Model configuration parameters</param>
+        public Yolov13DetModel(Yolov13DetConfig config) : base(config) { }
+
+
+        protected override DataTensor Preprocess(object img, out ImageAdjustmentParam imageAdjustmentParam)
+        {
+            MyLogger.Log.Debug($"开始{config.ModelType.ToString()}预处理流程，输入尺寸: {(img as Image<Rgb24>)?.Size()}");
+
+            try
+            {
+                return CvDataProcessor.ImageProcessToDataTensor(
+                    (Image<Rgb24>)img,
+                    config,
+                    out imageAdjustmentParam);
+            }
+            catch (Exception ex)
+            {
+                MyLogger.Log.Error($"预处理过程中发生异常: {ex.Message}", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/DeploySharp.OpenCvSharp/Model/ModelService/Yolo/Yolov13DetModel.cs
@@ -0,0 +1,40 @@
+﻿using DeploySharp.Data;
+using DeploySharp.Log;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeploySharp.Model
+{
+    public class Yolov13DetModel : IYolov13DetModel
+    {
+        /// <summary>
+        /// Constructor initializes with model configuration
+        /// </summary>
+        /// <param name="config">Model configuration parameters</param>
+        public Yolov13DetModel(Yolov13DetConfig config) : base(config) { }
+
+
+        protected override DataTensor Preprocess(object img, out ImageAdjustmentParam imageAdjustmentParam)
+        {
+            MyLogger.Log.Debug($"开始{config.ModelType.ToString()}预处理流程，输入尺寸: {(img as Mat)?.Size()}");
+
+            try
+            {
+                return CvDataProcessor.ImageProcessToDataTensor(
+                    (Mat)img,
+                    config,
+                    out imageAdjustmentParam);
+            }
+            catch (Exception ex)
+            {
+                MyLogger.Log.Error($"预处理过程中发生异常: {ex.Message}", ex);
+                throw;
+            }
+        }
+    }
+
+}
